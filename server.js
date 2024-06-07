ee6462719dc50c3e7aabfcbb7fe2437ec78a47d8
Petr Broz <petr.broz@autodesk.com>
2024-06-07 09:17:43 +0100

    Custom server error handling.

--- server.js
+++ server.js
@@ -111,4 +111,8 @@ app.post("/prompt/:urn", express.json(), async function (req, res, next) {
         next(err);
     }
 });
+app.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).send(err.message);
+});
 app.listen(PORT, function () { console.log(`Server listening on port ${PORT}...`); });
